9b6ded2a31f010a14358eefb2379224fa74c59c0
Deendayal Pareek <vishalpareek409@gmail.com>
2026-04-10 14:00:37 +0530

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -9,7 +9,7 @@ export default async function handler(req, res) {
 
   try {
     const response = await fetch(
-      "https://generativelanguage.googleapis.com/v1/models/gemini-1.5-flash:generateContent?key=" + API_KEY,
+      "https://generativelanguage.googleapis.com/v1/models/gemini-1.5-flash-002:generateContent?key=" + API_KEY,
       {
         method: "POST",
         headers: { "Content-Type": "application/json" },
